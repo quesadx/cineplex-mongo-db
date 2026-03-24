d01b1cf31dcf439e6fc925043d7ed24236cb26c4
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-03-24 11:54:45 -0600

    Update MongoDB connection URI in import script for consistency

--- script-importar-cineplex.sh
+++ script-importar-cineplex.sh
@@ -1,12 +1,12 @@
 # Se necesita mongodb-tools para ejecutar este script
-mongoimport --uri "mongodb+srv://hairol:hairol@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
   --collection cadenas --file cadenas.json --jsonArray
 
-mongoimport --uri "mongodb+srv://hairol:hairol@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
   --collection complejos --file complejos.json --jsonArray
 
-mongoimport --uri "mongodb+srv://hairol:hairol@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
   --collection salas --file salas.json --jsonArray
 
-mongoimport --uri "mongodb+srv://hairol:hairol@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
   --collection peliculas --file peliculas.json --jsonArray
