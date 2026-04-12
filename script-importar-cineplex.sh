da7d8865960e1243bc0721124528e4bee25fe3e2
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-04-12 01:19:56 -0600

    Update database schema in cineplex-dbdiagram and enhance import script with additional collections

--- script-importar-cineplex.sh
+++ script-importar-cineplex.sh
@@ -10,3 +10,28 @@ mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.n
 
 mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
   --collection peliculas --file peliculas.json --jsonArray
+
+# Avance 2
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection funciones --file funciones.json --jsonArray
+
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection horarios --file horarios.json --jsonArray
+
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection tarifas --file tarifas.json --jsonArray
+
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection asientos --file asientos.json --jsonArray
+
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection promociones --file promociones.json --jsonArray
+
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection clientes --file clientes.json --jsonArray
+
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection reservas --file reservas.json --jsonArray
+
+mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+  --collection boletos --file boletos.json --jsonArray
