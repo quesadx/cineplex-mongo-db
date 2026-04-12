3547ed9b10a3ba4aec8292b930d71126e003e0a6
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-04-12 01:30:19 -0600

    Add flake.nix and flake.lock for Nix package management; update import script with correct MongoDB URI

--- script-importar-cineplex.sh
+++ script-importar-cineplex.sh
@@ -1,37 +1,40 @@
 # Se necesita mongodb-tools para ejecutar este script
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+# IMPORTANTE: Set your MongoDB URI with credentials before running this script
+# You can export it as: export MONGODB_URI="mongodb+srv://<user>:<password>@cineplexcluster.xeej5kl.mongodb.net/cineplex_db"
+
+mongoimport --uri "${MONGODB_URI}" \
   --collection cadenas --file cadenas.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection complejos --file complejos.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection salas --file salas.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection peliculas --file peliculas.json --jsonArray
 
 # Avance 2
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection funciones --file funciones.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection horarios --file horarios.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection tarifas --file tarifas.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection asientos --file asientos.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection promociones --file promociones.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection clientes --file clientes.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection reservas --file reservas.json --jsonArray
 
-mongoimport --uri "mongodb+srv://user:password@cineplexcluster.xeej5kl.mongodb.net/cineplex_db" \
+mongoimport --uri "${MONGODB_URI}" \
   --collection boletos --file boletos.json --jsonArray
